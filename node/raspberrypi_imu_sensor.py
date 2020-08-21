6e3b88d2fb93022c4d88faf29df5efbc52ad1986
ParkJinSuk <qkrwlstjr9@naver.com>
2020-08-21 17:49:55 +0900

    test

--- node/raspberrypi_imu_sensor.py
+++ node/raspberrypi_imu_sensor.py
@@ -9,8 +9,8 @@ from mpu6050 import mpu6050
 sensor = mpu6050(0x68)
 
 def update_mpu6050():
-    rospy.loginfo("gyro data\t"sensor.get_gyro_data())
-    rospy.loginfo("accel data\t"sensor.get_accel_data())
+    rospy.loginfo("gyro data\t" + sensor.get_gyro_data())
+    rospy.loginfo("accel data\t" + sensor.get_accel_data())
 
 if __name__ == "__main__":
     rospy.init_node('imu_broadcaster')
