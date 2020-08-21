bdb295a00f9e2efd46857f05bd3a871d92b2c15e
ParkJinSuk <qkrwlstjr9@naver.com>
2020-08-21 19:38:55 +0900

    t

--- node/raspberrypi_imu_sensor.py
+++ node/raspberrypi_imu_sensor.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 import roslib
 import rospy
-
+import time
 import tf
 
 from mpu6050 import mpu6050
@@ -9,29 +9,37 @@ from sensor_msgs.msg import Imu
 
 sensor = mpu6050(0x68)
 
-float gyro_x
-float gyro_y
-float gyro_z
-float accel_x
-float accel_y
-float accel_z
+float gyro_x    = 0
+float gyro_y    = 0
+float gyro_z    = 0
+float accel_x   = 0
+float accel_y   = 0
+float accel_z   = 0
 
 def update_mpu6050():
-    gyro_data = sensor.get_gyro_data().split()
-    rospy.loginfo(gyro_data[0])
+    gyro_data = sensor.get_gyro_data().split('')
+    print(gyro_data[0])
+    print(gyro_data[1])
+    print(gyro_data[2])
     # rospy.loginfo("gyro data\t" + sensor.get_gyro_data())
     # rospy.loginfo("accel data\t" + sensor.get_accel_data())
 
-def node_imu_topic():
-    rospy.init_node('imu_broadcaster')
 
-    while not rospy.is_shutdown():
-        pub.publish()
+# def node_imu_topic():
+#     rospy.init_node('imu_broadcaster')
 
-        rospy.sleep(1.0)
+#     while not rospy.is_shutdown():
+#         pub.publish()
+
+#         rospy.sleep(1.0)
 
 if __name__ == "__main__":
     try:
-        node_imu_topic()
+        # node_imu_topic()
+
+        while(1):
+            update_mpu6050()
+            time.sleep(0.5)
+
     except rospy.ROSInterruptException:
         pass
